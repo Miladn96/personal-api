935a8bc23e934101a8158f15c4fd12e6f304658d
Milad Nasiri <milad@ecognitionlabs.com>
2022-08-07 21:11:04 +0430

    create message delete api

--- messages/successful-message.js
+++ messages/successful-message.js
@@ -2,12 +2,14 @@ const SUCCESS_MESSAGES = {
   createUserMessage:`User Created!`,
   getUser: `User found successfully!`,
   getUsers: `Users found successfully!`,
-  putNameMessage: `Message Send!`
+  putNameMessage: `Message Send!`,
+  deleteMessageSuccessful: `Message Deleted!`,
 }
 
 module.exports = {
   createUserMessage: SUCCESS_MESSAGES.createUserMessage,
   getUser: SUCCESS_MESSAGES.getUser,
   getUsers: SUCCESS_MESSAGES.getUsers,
-  putNewMessage_message: SUCCESS_MESSAGES.putNameMessage
+  putNewMessage_message: SUCCESS_MESSAGES.putNameMessage,
+  deleteMessageSuccessful: SUCCESS_MESSAGES.deleteMessageSuccessful,
 };
